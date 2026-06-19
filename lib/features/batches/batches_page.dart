e1f35da13fdcb255aea113e0fbe7635580838d11
Dozo <simpliminimalist@users.noreply.github.com>
2026-06-18 19:13:15 -0700

    rotes

--- lib/features/batches/batches_page.dart
+++ lib/features/batches/batches_page.dart
@@ -5,9 +5,13 @@ class BatchesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: const Center(
         child: Text('Batches Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
